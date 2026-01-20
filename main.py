b231a8214e5fd4e43d6176d2efd136c4bfa04e2b
PiggyPig <98620482+PiggyPigCute@users.noreply.github.com>
2026-01-20 03:50:04 +0100

    on_ready message

--- main.py
+++ main.py
@@ -148,6 +148,10 @@ class CatchView(discord.ui.View):
 @bot.event
 async def on_ready():
     await bot.tree.sync()
+    print("┌── Guilds where MicroBalls is")
+    for guild in bot.guilds:
+        print("│",guild.name)
+    print("└──")
     for emoji in bot.get_guild(EMOJI_GUILD_ID).emojis:
         emojis[emoji.name] = "<:"+emoji.name+":"+str(emoji.id)+"> "
     print("Let's go !")
